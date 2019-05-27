faa71bf85aace85e0fd2859d5810e1329b7e7245
aeustis <akeustis@gmail.com>
2019-05-26 23:44:05 -0700

    wip commit with phrase parsing

--- tokenizer/tokenizer.go
+++ tokenizer/tokenizer.go
@@ -2,8 +2,6 @@ package tokenizer
 
 import (
 	"bufio"
-	"fmt"
-	"os"
 	"strings"
 )
 
@@ -13,27 +11,66 @@ type Token struct {
 	Value interface{}
 }
 
+// Exported token types
 const (
-	TokHAI     = iota
-	TokKTHXBYE = iota
-	TokLiteral = iota
-	TokIdent   = iota
-	TokEOL     = iota
-	TokIHASA   = iota
-	TokITZ     = iota
+	TokErr = iota
+	TokHAI
+	TokKTHXBYE
+	TokLiteral
+	TokIdent
+	TokEOL
+	TokIHASA
+	TokITZ
+	TokR
+	TokIIZ
 )
 
-var phraseStore = make(map[string][]string)
-
-func keyPhrase(t TokenType, phr string) {
-
+type phraseNode struct {
+	t     TokenType
+	nodes map[string]*phraseNode
 }
-func phrases() {
-	keyPhrase(TokHAI, "HAI")
-	keyPhrase(TokKTHXBYE, "KTHXBYE")
-	keyPhrase(TokIHASA, "I HAS A")
-	keyPhrase(TokITZ, "ITZ")
+
+func newPhraseNode() *phraseNode {
+	return &phraseNode{TokErr, make(map[string]*phraseNode)}
 }
+
+type phraseInit struct {
+	t      TokenType
+	phrase string
+}
+
+func initPhrases(phraseInits []phraseInit) *phraseNode {
+	phraseRoot := newPhraseNode()
+	for _, init := range phraseInits {
+		initPhrase(phraseRoot, init.t, strings.Split(init.phrase, " "))
+	}
+	return phraseRoot
+}
+
+// Recursively build phrase tree
+func initPhrase(root *phraseNode, t TokenType, words []string) {
+	if len(words) == 0 {
+		root.t = t
+		return
+	}
+	word := words[0]
+	node := root.nodes[word]
+	if node == nil {
+		node = newPhraseNode()
+		root.nodes[word] = node
+	}
+	initPhrase(node, t, words[1:])
+}
+
+var phraseRoot = initPhrases([]phraseInit{
+	{TokEOL, "\n"},
+	{TokHAI, "HAI"},
+	{TokKTHXBYE, "KTHXBYE"},
+	{TokIHASA, "I HAS A"},
+	{TokITZ, "ITZ"},
+	{TokR, "R"},
+	{TokIIZ, "I IZ"},
+})
 
 // emitFragments reads from a bufio.Reader and emits string fragments on the given channel,
 // omitting all comments and converting line separators "," into "\n" fragments
@@ -51,44 +88,98 @@ txtLine:
 			if len(fragments) == 0 {
 				continue //ignore empty lines
 			}
-			i := 0
-			switch fragments[0] {
-			// OBTW and TLDR are required to be the first token on a Lolcode line, per spec
-			case "OBTW":
-				insideComment = true
-			case "TLDR":
-				insideComment = false
-				i = 1 // Resume processing after the TLDR
-			case "BTW":
-				continue txtLine // Don't emit an extra "\n" for full-line comment
-			}
-			if !insideComment {
-				for ; i < len(fragments); i++ {
-					if fragments[i] == "BTW" {
-						out <- "\n"
-						continue txtLine
-					}
-					out <- fragments[i]
+			start := 0
+			if insideComment {
+				if fragments[0] != "TLDR" {
+					continue
 				}
-				out <- "\n"
+				insideComment = false
+				if len(fragments) == 1 {
+					continue
+				}
+				start = 1
+			} else {
+				if fragments[0] == "OBTW" {
+					insideComment = true
+					continue
+				}
+			}
+			for i := start; i < len(fragments); i++ {
+				if fragments[i] == "BTW" {
+					if i > start {
+						out <- "\n"
+					}
+					continue txtLine
+				}
+				out <- fragments[i]
+			}
+			out <- "\n"
+		}
+	}
+}
+
+// EmitTokens parses a Lolcode reader and emits a stream of Tokens
+func EmitTokens(reader *bufio.Reader, out chan<- Token) {
+	defer close(out)
+	frags := make(chan string, 100)
+	go emitFragments(reader, frags)
+	for word := range frags {
+		// Emit as many phrase tokens as we can
+		if word, ok := emitPhraseToken(word, frags, out); !ok {
+			switch {
+			case word == "": // end of input
+				return
+			case word[0] == '"': // string literal
+				out <- stringLiteralToToken(word)
 			}
 		}
 	}
 }
 
-// Parse echoes each line of stdin back out
-func Parse() {
-	phrases()
-	stdin := bufio.NewReader(os.Stdin)
+// Reads a phrase starting with the given fragment (word)
+// uses single-word look-ahead to parse as long a phrase as possible
+func emitPhraseToken(word string, frags <-chan string, out chan<- Token) (string, bool) {
+	phraseNode := phraseRoot
+	hasRead := false
 	for {
-		text, err := stdin.ReadString('\n')
-		if err != nil {
-			return
+		if phraseNode = phraseNode.nodes[word]; phraseNode == nil {
+			if hasRead {
+				token := Token{phraseNode.t, nil}
+				if token.Type == TokErr {
+					// If we have an TokError, fill in a parser error as the value
+					token.Value = getErrMessageForPhrase(phraseNode, word)
+				}
+				out <- token
+				return word, true
+			} //else
+			return word, false
 		}
-		tokens := strings.Fields(text)
-		for _, token := range tokens {
-			fmt.Printf("%s,", token)
-		}
-		fmt.Println()
+		hasRead = true
+		word = <-frags
 	}
+}
+
+// TODO: add string escaping
+func stringLiteralToToken(str string) Token {
+	// String literal must end with '"' and have length at least 2
+	if l := len(str); l < 2 || str[l-1] != '"' {
+		return Token{TokErr, "Invalid string literal: " + str}
+	}
+	// chop off the start and end quotes
+	return Token{TokLiteral, str[1 : len(str)-1]}
+}
+
+func getErrMessageForPhrase(node *phraseNode, word string) string {
+	var out strings.Builder
+	w := out.WriteString
+	w("Syntax error at ")
+	w(word)
+	w(": expected ")
+	sep := ""
+	for word := range node.nodes {
+		w(sep)
+		w(word)
+		sep = " or "
+	}
+	return out.String()
 }
--- tokenizer/tokenizer_test.go
+++ tokenizer/tokenizer_test.go
@@ -6,25 +6,28 @@ import (
 	"testing"
 )
 
+const lolCode = `HAI 1.2
+tok0,tok1 BTW comments here, including some commas
+tok2
+
+	BTW full line comment
+tok3 BTW, OBTW doesnt work here
+tok4	 OBTW   illegal comment
+tok5, OBTW legal comment,, TLDR tok6,,,
+tok7, OBTW legal comment TLDR
+BTW that TLDR doesn't work lulz, TLDR BTW
+KTHXBYE
+`
+
 func TestEmitFragments(t *testing.T) {
-	const lolCode = `HAI 1.2
-	tok1 BTW comments here, including some commas
-	tok2
-	BTW, full line comment
-	tok3 BTW
-	tok4	 OBTW   illegal comment
-	tok5, OBTW legal comment TLDR
-	BTW that TLDR didn't work lulz, TLDR
-	that didn't work either lulz, OBTW, TLDR tok6
-	KTHXBYE
-	`
 	expected := []string{"HAI", "1.2", "\n",
-		"tok1", "\n",
+		"tok0", "\n", "tok1", "\n",
 		"tok2", "\n",
 		"tok3", "\n",
 		"tok4", "OBTW", "illegal", "comment", "\n",
 		"tok5", "\n",
 		"tok6", "\n",
+		"tok7", "\n",
 		"KTHXBYE", "\n"}
 	reader := bufio.NewReader(strings.NewReader(lolCode))
 	fragments := make(chan string, 100)
@@ -33,6 +36,37 @@ func TestEmitFragments(t *testing.T) {
 	for fragment := range fragments {
 		if fragment != expected[i] {
 			t.Fatalf("Expected: %s Got: %s", expected[i], fragment)
+		}
+		i++
+	}
+	if i != len(expected) {
+		t.Fatalf("Expected %d tokens, got %d", len(expected), i)
+	}
+}
+
+const lolCode2 = `HAI 1.2
+I HAS A FISH ITZ 5
+BTW full line comment
+OBTW,TLDR 
+FISH R "foo"
+KTHXBYE
+
+`
+
+func TestEmitTokens(t *testing.T) {
+	expected := []Token{
+		{TokHAI, nil}, {TokLiteral, float32(1.2)}, {TokEOL, nil},
+		{TokIHASA, nil}, {TokIdent, "FISH"}, {TokITZ, nil}, {TokLiteral, int(5)}, {TokEOL, nil},
+		{TokIdent, "FISH"}, {TokR, nil}, {TokLiteral, "foo"}, {TokEOL, nil},
+		{TokKTHXBYE, nil}, {TokEOL, nil},
+	}
+	reader := bufio.NewReader(strings.NewReader(lolCode2))
+	fragments := make(chan Token, 100)
+	go EmitTokens(reader, fragments)
+	i := 0
+	for fragment := range fragments {
+		if fragment != expected[i] {
+			t.Fatalf("Expected: %v Got: %v", expected[i], fragment)
 		}
 		i++
 	}
