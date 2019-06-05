eecf5038c315863256449f7dc847048b6552438e
aeustis <akeustis@gmail.com>
2019-06-04 20:29:21 -0700

    math expressions

--- lang/compile.go
+++ lang/compile.go
@@ -1,5 +1,10 @@
 package lang
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type namespace struct {
 	vars map[string]interface{}
 }
@@ -84,18 +89,77 @@ func ident(args []interface{}) interface{} {
 	}
 }
 
+func getNumericValue(val interface{}, useFloat bool) interface{} {
+	typePanicMsg := "Cannot perform numerical operation on type "
+	switch v := val.(type) {
+	case nil:
+		panic(typePanicMsg + "NOOB")
+	case bool:
+		panic(typePanicMsg + "TROOF")
+	case int64:
+		if useFloat {
+			return float64(v)
+		}
+		return v
+	case float64:
+		return v
+	case string:
+		if !useFloat {
+			if i, err := strconv.ParseInt(v, 0, 64); err == nil {
+				return i
+			}
+		}
+		if f, err := strconv.ParseFloat(v, 64); err == nil {
+			return f
+		}
+		panic("Failed to parse numeric value from string: " + v)
+	default:
+		panic(typePanicMsg + fmt.Sprintf("%T", v))
+	}
+}
+
+func makeMathExpr(left, right expr, intOper func(int64, int64) int64,
+	floatOper func(float64, float64) float64) expr {
+	return func(ns *namespace) interface{} {
+		switch v1 := getNumericValue(left(ns), false).(type) {
+		case int64:
+			switch v2 := getNumericValue(right(ns), false).(type) {
+			case int64:
+				return intOper(v1, v2)
+			default: // float64
+				return floatOper(float64(v1), v2.(float64))
+			}
+		default: // float64
+			v2 := getNumericValue(right(ns), true)
+			return floatOper(v1.(float64), v2.(float64))
+		}
+	}
+}
+
 func sumofXAnY(args []interface{}) interface{} {
-	return nil
+	return makeMathExpr(args[1].(expr), args[3].(expr),
+		func(a, b int64) int64 { return a + b },
+		func(a, b float64) float64 { return a + b },
+	)
 }
 
 func diffofXAnY(args []interface{}) interface{} {
-	return nil
+	return makeMathExpr(args[1].(expr), args[3].(expr),
+		func(a, b int64) int64 { return a - b },
+		func(a, b float64) float64 { return a - b },
+	)
 }
 
 func prodofXAnY(args []interface{}) interface{} {
-	return nil
+	return makeMathExpr(args[1].(expr), args[3].(expr),
+		func(a, b int64) int64 { return a * b },
+		func(a, b float64) float64 { return a * b },
+	)
 }
 
 func quoshofXAnY(args []interface{}) interface{} {
-	return nil
+	return makeMathExpr(args[1].(expr), args[3].(expr),
+		func(a, b int64) int64 { return a / b },
+		func(a, b float64) float64 { return a / b },
+	)
 }
